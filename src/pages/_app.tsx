24b5a8ed936ceddd4e14b12eb502352a795d1c76
olukukoyi <oluwanifesimi.kukoyi15@gmail.com>
2023-03-02 00:53:27 -0500

    second change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
+
+// stopped @5:54
+// https://www.youtube.com/watch?v=dXRRY37MPuk
 
 import { api } from "~/utils/api";
 
